3bc731a834cda783ed44590ade7827c5f6e933e7
Kristian Tryggestad <kristian.tryggestad@gmail.com>
2011-12-29 03:13:54 +0100

    Added a script for minifying the JavaScript used and referenced the minified versions on the index-page

---
+++ minifyjs.sh
@@ -0,0 +1,13 @@
+lib/uglifyjs/bin/uglifyjs -nc --unsafe --lift-vars -o public_html/js/main.min.js public_html/js/main.js
+
+lib/uglifyjs/bin/uglifyjs -nc --unsafe --lift-vars -o public_html/js/flux.min.js public_html/js/flux.js
+
+lib/uglifyjs/bin/uglifyjs -nc --unsafe --lift-vars -o public_html/js/bootstrap-alerts.min.js public_html/js/bootstrap-alerts.js
+lib/uglifyjs/bin/uglifyjs -nc --unsafe --lift-vars -o public_html/js/bootstrap-twipsy.min.js public_html/js/bootstrap-twipsy.js
+lib/uglifyjs/bin/uglifyjs -nc --unsafe --lift-vars -o public_html/js/bootstrap-popover.min.js public_html/js/bootstrap-popover.js
+
+lib/uglifyjs/bin/uglifyjs -nc --unsafe --lift-vars -o public_html/js/jquery.prettydate.min.js public_html/js/jquery.prettydate.js
+
+lib/uglifyjs/bin/uglifyjs -nc --unsafe --lift-vars -o public_html/js/local/flickr.min.js public_html/js/local/flickr.js
+lib/uglifyjs/bin/uglifyjs -nc --unsafe --lift-vars -o public_html/js/local/twitter.min.js public_html/js/local/twitter.js
+lib/uglifyjs/bin/uglifyjs -nc --unsafe --lift-vars -o public_html/js/local/lastfm.min.js public_html/js/local/lastfm.js
